02b4946f0e1f622e9fdb9385a9cab1ddd06da66b
Nico Jansen <jansennico@gmail.com>
2019-02-11 10:54:38 +0100

    feat(dispose): Add functionality to explicit disposing of dependencies (#1)
    Add the `dispose` method. Calling `dispose` on an `Injector` automatically disposes all dependencies created by the injector. It also disposes the parent injectors.
    
    See readme for more details.

--- test/unit/Injector.spec.ts
+++ test/unit/Injector.spec.ts
@@ -5,6 +5,8 @@ import { rootInjector } from '../../src/InjectorImpl';
 import { TARGET_TOKEN, INJECTOR_TOKEN } from '../../src/api/InjectionToken';
 import { Exception } from '../../src/Exception';
 import { Scope } from '../../src/api/Scope';
+import * as sinon from 'sinon';
+import { Disposable } from '../../src/api/Disposable';
 
 describe('InjectorImpl', () => {
 
@@ -132,54 +134,10 @@ describe('InjectorImpl', () => {
       const actualFoo = barBazInjector.injectClass(Foo);
       expect(actualFoo.injector).eq(barBazInjector);
     });
+
   });
 
-  describe('ValueInjector', () => {
-    it('should be able to provide a value', () => {
-      const sut = rootInjector.provideValue('foo', 42);
-      const actual = sut.injectClass(class {
-        constructor(public foo: number) { }
-        public static inject = tokens('foo');
-      });
-      expect(actual.foo).eq(42);
-    });
-    it('should be able to provide a value from the parent injector', () => {
-      const sut = rootInjector
-        .provideValue('foo', 42)
-        .provideValue('bar', 'baz');
-      expect(sut.resolve('bar')).eq('baz');
-      expect(sut.resolve('foo')).eq(42);
-    });
-  });
-
-  describe('FactoryInjector', () => {
-    it('should be able to provide the return value of the factoryMethod', () => {
-      const expectedValue = { foo: 'bar' };
-      function foobar() {
-        return expectedValue;
-      }
-
-      const actual = rootInjector
-        .provideFactory('foobar', foobar)
-        .injectClass(class {
-          constructor(public foobar: { foo: string }) { }
-          public static inject = tokens('foobar');
-        });
-      expect(actual.foobar).eq(expectedValue);
-    });
-
-    it('should be able to provide parent injector values', () => {
-      function answer() {
-        return 42;
-      }
-      const factoryInjector = rootInjector.provideFactory('answer', answer);
-      const actual = factoryInjector.injectClass(class {
-        constructor(public injector: Injector<{ answer: number }>, public answer: number) { }
-        public static inject = tokens(INJECTOR_TOKEN, 'answer');
-      });
-      expect(actual.injector).eq(factoryInjector);
-      expect(actual.answer).eq(42);
-    });
+  describe('ChildInjector', () => {
 
     it('should cache the value if scope = Singleton', () => {
       // Arrange
@@ -222,6 +180,207 @@ describe('InjectorImpl', () => {
       // Assert
       expect(first.count).eq(0);
       expect(second.count).eq(1);
+    });
+  });
+
+  describe('ValueProvider', () => {
+    it('should be able to provide a value', () => {
+      const sut = rootInjector.provideValue('foo', 42);
+      const actual = sut.injectClass(class {
+        constructor(public foo: number) { }
+        public static inject = tokens('foo');
+      });
+      expect(actual.foo).eq(42);
+    });
+    it('should be able to provide a value from the parent injector', () => {
+      const sut = rootInjector
+        .provideValue('foo', 42)
+        .provideValue('bar', 'baz');
+      expect(sut.resolve('bar')).eq('baz');
+      expect(sut.resolve('foo')).eq(42);
+    });
+    it('should throw after disposed', () => {
+      const sut = rootInjector
+        .provideValue('foo', 42);
+      sut.dispose();
+      expect(() => sut.resolve('foo')).throws('Injector is already disposed. Please don\'t use it anymore. Tried to resolve "foo".');
+      expect(() => sut.injectClass(class Bar { })).throws('Injector is already disposed. Please don\'t use it anymore. Tried to inject "Bar".');
+      expect(() => sut.injectFunction(function baz() { })).throws('Injector is already disposed. Please don\'t use it anymore. Tried to inject "baz".');
+    });
+  });
+
+  describe('FactoryProvider', () => {
+    it('should be able to provide the return value of the factoryMethod', () => {
+      const expectedValue = { foo: 'bar' };
+      function foobar() {
+        return expectedValue;
+      }
+
+      const actual = rootInjector
+        .provideFactory('foobar', foobar)
+        .injectClass(class {
+          constructor(public foobar: { foo: string }) { }
+          public static inject = tokens('foobar');
+        });
+      expect(actual.foobar).eq(expectedValue);
+    });
+
+    it('should be able to provide parent injector values', () => {
+      function answer() {
+        return 42;
+      }
+      const factoryProvider = rootInjector.provideFactory('answer', answer);
+      const actual = factoryProvider.injectClass(class {
+        constructor(public injector: Injector<{ answer: number }>, public answer: number) { }
+        public static inject = tokens(INJECTOR_TOKEN, 'answer');
+      });
+      expect(actual.injector).eq(factoryProvider);
+      expect(actual.answer).eq(42);
+    });
+
+    it('should throw after disposed', () => {
+      const sut = rootInjector.provideFactory('answer', function answer() {
+        return 42;
+      });
+      sut.dispose();
+      expect(() => sut.resolve('answer')).throws('Injector is already disposed. Please don\'t use it anymore. Tried to resolve "answer".');
+      expect(() => sut.injectClass(class Bar { })).throws('Injector is already disposed. Please don\'t use it anymore. Tried to inject "Bar".');
+      expect(() => sut.injectFunction(function baz() { })).throws('Injector is already disposed. Please don\'t use it anymore. Tried to inject "baz".');
+    });
+  });
+
+  describe('ClassProvider', () => {
+    it('should throw after disposed', () => {
+      const sut = rootInjector.provideClass('foo', class Foo { });
+      sut.dispose();
+      expect(() => sut.resolve('foo')).throws('Injector is already disposed. Please don\'t use it anymore. Tried to resolve "foo".');
+      expect(() => sut.injectClass(class Bar { })).throws('Injector is already disposed. Please don\'t use it anymore. Tried to inject "Bar".');
+      expect(() => sut.injectFunction(function baz() { })).throws('Injector is already disposed. Please don\'t use it anymore. Tried to inject "baz".');
+    });
+  });
+
+  describe('dispose', () => {
+
+    it('should dispose all disposable singleton dependencies', () => {
+      // Arrange
+      class Foo {
+        public dispose2 = sinon.stub();
+        public dispose = sinon.stub();
+      }
+      function barFactory(): Disposable & { dispose3(): void; } {
+        return { dispose: sinon.stub(), dispose3: sinon.stub() };
+      }
+      class Baz {
+        constructor(public readonly bar: Disposable & { dispose3(): void; }, public readonly foo: Foo) { }
+        public static inject = tokens('bar', 'foo');
+      }
+      const bazInjector = rootInjector
+        .provideClass('foo', Foo)
+        .provideFactory('bar', barFactory);
+      const baz = bazInjector
+        .injectClass(Baz);
+
+      // Act
+      bazInjector.dispose();
+
+      // Assert
+      expect(baz.bar.dispose).called;
+      expect(baz.foo.dispose).called;
+      expect(baz.foo.dispose2).not.called;
+      expect(baz.bar.dispose3).not.called;
+    });
+
+    it('should also dispose transient dependencies', () => {
+      class Foo { public dispose = sinon.stub(); }
+      function barFactory(): Disposable { return { dispose: sinon.stub() }; }
+      class Baz {
+        constructor(public readonly bar: Disposable, public readonly foo: Foo) { }
+        public static inject = tokens('bar', 'foo');
+      }
+      const bazInjector = rootInjector
+        .provideClass('foo', Foo, Scope.Transient)
+        .provideFactory('bar', barFactory, Scope.Transient);
+      const baz = bazInjector
+        .injectClass(Baz);
+
+      // Act
+      bazInjector.dispose();
+
+      // Assert
+      expect(baz.bar.dispose).called;
+      expect(baz.foo.dispose).called;
+    });
+
+    it('should dispose dependencies in correct order', () => {
+      class Foo { public dispose = sinon.stub(); }
+      class Bar { public dispose = sinon.stub(); }
+      class Baz {
+        constructor(public readonly bar: Bar, public readonly foo: Foo) { }
+        public static inject = tokens('bar', 'foo');
+        public dispose = sinon.stub();
+      }
+      const bazProvider = rootInjector
+        .provideClass('foo', Foo, Scope.Transient)
+        .provideClass('bar', Bar)
+        .provideClass('baz', Baz);
+      const baz = bazProvider.resolve('baz');
+      const newFoo = bazProvider.resolve('foo');
+
+      // Act
+      bazProvider.dispose();
+
+      // Assert
+      expect(baz.foo.dispose).calledBefore(baz.bar.dispose);
+      expect(newFoo.dispose).calledBefore(baz.bar.dispose);
+      expect(baz.bar.dispose).calledBefore(baz.dispose);
+    });
+
+    it('should not dispose injected classes or functions', () => {
+      class Foo { public dispose = sinon.stub(); }
+      function barFactory(): Disposable { return { dispose: sinon.stub() }; }
+      const foo = rootInjector.injectClass(Foo);
+      const bar = rootInjector.injectFunction(barFactory);
+      rootInjector.dispose();
+      expect(foo.dispose).not.called;
+      expect(bar.dispose).not.called;
+    });
+
+    it('should not dispose providedValues', () => {
+      const disposable: Disposable = { dispose: sinon.stub() };
+      const disposableProvider = rootInjector.provideValue('disposable', disposable);
+      disposableProvider.resolve('disposable');
+      disposableProvider.dispose();
+      expect(disposable.dispose).not.called;
+    });
+
+    it('should not break on non-disposable dependencies', () => {
+      class Foo { public dispose = true; }
+      function barFactory(): { dispose: string } { return { dispose: 'no-fn' }; }
+      class Baz {
+        constructor(public readonly bar: { dispose: string }, public readonly foo: Foo) { }
+        public static inject = tokens('bar', 'foo');
+      }
+      const bazInjector = rootInjector
+        .provideClass('foo', Foo)
+        .provideFactory('bar', barFactory);
+      const baz = bazInjector
+        .injectClass(Baz);
+
+      // Act
+      bazInjector.dispose();
+
+      // Assert
+      expect(baz.bar.dispose).eq('no-fn');
+      expect(baz.foo.dispose).eq(true);
+    });
+
+    it('should not dispose dependencies twice', () => {
+      const fooProvider = rootInjector
+        .provideClass('foo', class Foo implements Disposable { public dispose = sinon.stub(); });
+      const foo = fooProvider.resolve('foo');
+      fooProvider.dispose();
+      fooProvider.dispose();
+      expect(foo.dispose).calledOnce;
     });
   });
 
