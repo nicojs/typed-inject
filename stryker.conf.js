28c1d838d2225c6fd0be5c11bbc8d0b0c28b8ab3
Nico Jansen <nico.jansen@infosupport.com>
2019-04-26 18:22:33 +0200

    build: update dependencies, build and configuration

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,13 +1,22 @@
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-    mutator: "typescript",
-    packageManager: "npm",
-    reporters: ["html", "clear-text", "progress", "dashboard"],
-    testRunner: "mocha",
-    transpilers: ["typescript"],
-    testFramework: "mocha",
-    coverageAnalysis: "perTest",
-    tsconfigFile: "tsconfig.json",
-    mutate: ["src/**/*.ts", "!src/**/*.d.ts"]
+    mutator: 'typescript',
+    packageManager: 'npm',
+    reporters: ['html', 'clear-text', 'progress', 'dashboard'],
+    testRunner: 'mocha',
+    transpilers: ['typescript'],
+    testFramework: 'mocha',
+    coverageAnalysis: 'perTest',
+    tsconfigFile: 'tsconfig.lint.json',
+    mutate: ['src/**/*.ts', '!src/**/*.d.ts'],
+    mochaOptions: {
+      spec: ['test/helpers/**/*.js', 'test/unit/**/*.js']
+    },
+    maxConcurrentTestRunners: 2,
+    thresholds: {
+      high: 90,
+      low: 80,
+      break: 92
+    }
   });
 };
