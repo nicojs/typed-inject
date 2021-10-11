1a14d3a007a78efc76bd2f455c16885642ff4303
Nico Jansen <jansennico@gmail.com>
2021-10-11 10:18:12 +0200

    fix linting warning

--- test/integration/memory-leak.it.spec.ts
+++ test/integration/memory-leak.it.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { execSync } from 'child_process';
 describe('typed-inject memory', () => {
-  it('should not leak memory when creating and disposing child injectors', async () => {
+  it('should not leak memory when creating and disposing child injectors', () => {
     expect(() =>
       execSync('node --max-old-space-size=100  memory-leak-worker.js', {
         cwd: __dirname,
