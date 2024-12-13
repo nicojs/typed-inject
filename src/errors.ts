012c6d6bdd9ac06fc00c8a990b5e6573f00797ac
Nico Jansen <jansennico@gmail.com>
2024-12-13 21:05:32 +0100

    Update typescript config to use best practices

--- src/errors.ts
+++ src/errors.ts
@@ -47,10 +47,11 @@ export class InjectorDisposedError extends TypedInjectError {
 export class InjectionError extends TypedInjectError {
   constructor(
     public readonly path: InjectionTarget[],
-    public readonly cause: Error,
+    cause: unknown,
   ) {
     super(
-      `Could not ${describeInjectAction(path[0])} ${path.map(name).join(' -> ')}. Cause: ${cause.message}`,
+      `Could not ${describeInjectAction(path[0])} ${path.map(name).join(' -> ')}.${cause instanceof Error ? ` Cause: ${cause.message}` : ''}`,
+      { cause },
     );
   }
 
